9014c97bc21a9482ba9f0e0b6123fa538fcee8de
lainbash <43439023+lainbash@users.noreply.github.com>
2021-01-31 01:49:00 +0100

    Update demo.rkt

--- demo.rkt
+++ demo.rkt
@@ -1,4 +1,4 @@
-#lang sicp 
+#lang scheme
 
 ;; On peut implémenter pas mal de nos choses nous même en scheme, ce qui en fait un langage très ludique à utiliser
 ;; L'opérateur de comparaison >= n'existe pas par exemple, mais on peut l'implémenter nous même!
@@ -8,33 +8,60 @@
 ;;    true))
 ;; le langage peut être étendu très facilement, selon les besoins et envies du développeur !
 
+
+(define (<= x y)
+    (cond [(not (> x y)) true]
+          [else false]
+    )
+)
+
+
+(define (>= x y)
+    (cond  [(not (< x y)) true]
+           [else false]
+    )
+)
+
+
 (define (++ x)
-    (+ x 1))
+    (+ x 1)
+)
 
 (define (-- x)
-    (- x 1))
+    (- x 1)
+)
 
 
-(define x 2)
-(define y 1)
-(define max 3)
+(define (abs x)
+    (cond [(x < 0) (- x)]
+           [else x]
+    )
+)
 
-(++ 2)
-(-- 3)
-(++ x)
 
+(define (**-iter x y count)
+    (cond [(= count y) x]
+          [else (**-iter (* x x) y (+ count 1))]
+    )
+)
+
+
+(define (** x y)
+    (**-iter x y 1)
+)
 
 (define (while condition body)
-    (if (condition)
-         ((body) (while condition body))
+    (cond [(condition) (body)]
+          [else (while condition body)]
+    )
+)
 
-    ))
 
 (define (for condition body count)
-    (if (condition)
-        ((body) (for condition body count)
-
-        )))
+    (cond [(condition) (body)]
+          [ else (for condition body count)]
+    )
+)
 
 
 ;; On ne peut pas passer de prédicats aux procédure while et for, en principe ça marcherait 
